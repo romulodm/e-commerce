225a3722025631cd282628f1a5f8f5b4b447d110
Romulo <romulotg12@gmail.com>
2023-05-13 02:16:07 -0300

    bug fixed on login

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -4,8 +4,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
     registerUser: async(req, res) => {
-        console.log(req.body.username,req.body.email);
-
         let newUser = {
             username: req.body.username,
             email: req.body.email,
@@ -25,38 +23,41 @@ module.exports = {
 
     loginUser: async (req, res) => {
         try{
+            let json = {error:'', result:{}}
+
             const user = await AuthService.findUser(req.body.username);
     
-            !user && res.status(401).json("Wrong User Name");
-        
-            const hashedPassword = CryptoJS.AES.decrypt(
-                user.password,
-                process.env.PASS_SEC
-            );
-    
+            if (!user) {
+                console.log("entrei")
+                json.error = "Invalid credentials!"
+                return res.status(401).json({json});
+              }
+
+            const hashedPassword = CryptoJS.AES.decrypt(user.Password, process.env.PASS_SEC);
             const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-    
-            const inputPassword = req.body.password;
-            
-            originalPassword != inputPassword && 
-                res.status(401).json("Wrong Password");
+
+            if (originalPassword !== req.body.password) {
+                json.error = "Wrong password!"
+                return res.status(401).json({json});
+              }
     
             const accessToken = jwt.sign(
             {
-                id: user._id,
+                id: user.id,
                 isAdmin: user.isAdmin,
             },
-            process.env.JWT_SEC,
+                process.env.JWT_SEC,
                 {expiresIn:"3d"}
             );
-      
-            const { password, ...others } = user._doc;  
-            res.status(200).json({...others, accessToken});
-    
-        }catch(err){
-            res.status(500).json(err);
+
+            const { password, ...others } = user;  
+                  
+            json.result = { ...others, accessToken };
+            res.status(200).json({json})
+            
+        }catch(error){
+            res.status(500).json(error);
         }
-    
     }
 
 }
--- api/services/AuthService.js
+++ api/services/AuthService.js
@@ -2,7 +2,6 @@ const DB = require("../db");
 
 module.exports = {
     registerUser: (user) => {
-        console.log("toaquiiiiiii")
         return new Promise((acepted, rejected) => {
             DB.query('INSERT INTO users (username, password, email) values (?,?,?)', 
             
@@ -33,26 +32,5 @@ module.exports = {
                 }
             })
         });
-    },
-
-    getId: (username) => {
-        return new Promise((aceito, rejeitado) => {
-            DB.query('SELECT Id FROM users WHERE username = ?', [username], (error, results) => {
-                if(error) {
-                    rejeitado(error); 
-                    return;
-                }
-                if(results.length > 0){
-                    aceito(results[0]);
-                }
-                else{
-                    aceito(false);
-                }
-            })
-        });
-    },
-
-
-
-
+    }
 }
