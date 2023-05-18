c8490daace9ff32f4c5d3bd973d406d42aa6557d
Romulo de Moraes <romulotg12@gmail.com>
2023-05-18 12:37:11 -0300

    app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -23,10 +23,8 @@ const App = () => {
         <Route path="/products/:category" element={<ProductList />}/>
         <Route path="/product/:id" element={<Product />}/>
         <Route path="/cart" element={<Cart />}/>
-
         <Route path="/login" element={user ? <Navigate to="/" />  : <Login />}/>
         <Route path="/register" element={user ? <Navigate to="/" />  : <Register />}/>
-
 
       </Routes>
     </Router>
